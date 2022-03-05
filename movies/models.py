3583214ef457cc2ef9c08cb6f18c1ed4415a2f9f
Bubuclem <clement.bubuyt@gmail.com>
2022-03-05 20:33:19 +0100

    Updates

--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,39 @@
+from statistics import mode
 from django.db import models
 
-# Create your models here.
+class Utilisateur(models.Model):
+    id      = models.IntegerField(primary_key=True)
+    token   = models.UUIDField()
+    email   = models.EmailField()
+
+class Video(models.Model):
+    id = models.IntegerField(primary_key=True)
+
+class People(models.Model):
+    id = models.IntegerField(primary_key=True)
+
+class Genre(models.Model):
+    id = models.IntegerField(primary_key=True)
+
+class Media(models.Model):
+    id                  = models.IntegerField(primary_key=True)
+    overview            = models.CharField(max_length=200)
+    poster_path         = models.CharField(max_length=200)
+    popularity          = models.FloatField(max_length=200)
+    backdrop_path       = models.CharField(max_length=200)
+    vote_average        = models.FloatField()
+    release_date        = models.CharField(max_length=200)
+    original_language   = models.CharField(max_length=200)
+    genre_ids           = models.JSONField(max_length=200)
+    vote_count          = models.IntegerField()
+    title               = models.CharField(max_length=200)
+    original_title      = models.CharField(max_length=200)
+    origin_country      = models.JSONField()
+    video               = models.BooleanField()
+    adult               = models.BooleanField()
+
+    budget              = models.IntegerField()
+    revenue             = models.IntegerField()
+    runtime             = models.IntegerField()
+    status              = models.CharField(max_length=200)
+    tagline             = models.CharField(max_length=200)
